28b790e5ab1d7945157e4945ec91e3065cab9387
Antonio Magana <39139814+AntonioMagana@users.noreply.github.com>
2020-04-28 15:25:41 -0700

    Merged with Master

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import flask_login
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, send_file
 from flask_login import current_user, login_required, login_user, logout_user
 from flask_login import LoginManager
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash
+from io import BytesIO
 
 from forms import RegistrationForm, LoginForm
 from flask_bootstrap import Bootstrap
@@ -22,9 +23,11 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +39,7 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f'<user: {self.username}>'
 
+
 class ResumeList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
@@ -44,6 +48,7 @@ class ResumeList(db.Model):
 
     def __repr__(self):
         return f'<post: {self.text}>'
+
 
 @app.route("/")
 @app.route("/home")
@@ -64,17 +69,30 @@ def register():
         return redirect(url_for('dashboard'))
     return render_template('register.html', form=form)
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
-           if form.password.data == user.password:
-              login_user(user, remember=form.remember.data)
-              return redirect(url_for('dashboard'))
+            if form.password.data == user.password:
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
         flash('Incorrect username/password. Try again.')
     return render_template('login.html', form=form)
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/selection')
+def selection():
+    return render_template('selection.html')
+
 
 @app.route('/save', methods=['POST'])
 def save():
@@ -84,10 +102,13 @@ def save():
     db.session.commit()
     return redirect(url_for('home'))
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
+
+@app.route('/download')
+def download():
+    file_data = ResumeList.query.filter_by(id=1).first()
+    return send_file(BytesIO(file_data.resume), attachment_filename='flask.pdf', as_attachment=True)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
